a2389f129f051257a05cb7d5ce856938c623b571
abidsoft <abid_soft@hotmail.com>
2016-02-26 02:16:31 +0500

    Update index.js

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', function(req, res) {
   res.status(200).send('I dream of being a web site.');
 });
 
-var port = process.env.PORT || 1337;
+var port = process.env.PORT || 1338;
 app.listen(port, function() {
     console.log('parse-server-example running on port ' + port + '.');
 });
